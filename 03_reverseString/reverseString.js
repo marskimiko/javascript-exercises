88188fe04ffaecfdb55a07a300fd0c76a4b0a051
Marisa Saunders <marisaksaunders@gmail.com>
2024-01-29 15:11:29 -0500

    pass all reverse string tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function() {
+// const reverseString = function(string) {
+//   let splitString = string.split('');
+//   let reverseArray = splitString.reverse();
+//   let joinArray = reverseArray.join('');
+//   return joinArray
+// };
 
+const reverseString = function(string) {
+  let newString = "";
+
+  for (let i = string.length - 1; i >= 0; i--) {
+    newString += string[i];
+  }
+  return newString;
 };
 
 // Do not edit below this line
