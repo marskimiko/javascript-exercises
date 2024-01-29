e0c5e432282a3cbde1d00d025f8e8606b1ef48b7
Marisa Saunders <marisaksaunders@gmail.com>
2024-01-29 15:56:13 -0500

    utilized rest operator on argument

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+// const removeFromArray = function(arr, arg) {
+//   const cleanArray = arr.filter(function (letter) {
+//     return letter !== arg;
+//   })
+//   return cleanArray;
+// };
 
+const removeFromArray = function(arr, ...arg) {
+  // utilized rest operator
+  const filteredArray = arr.filter((item) => {
+    return !arg.includes(item);
+  })
+  return filteredArray;
 };
 
 // Do not edit below this line
